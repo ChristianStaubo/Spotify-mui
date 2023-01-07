5a23ee078c18530e35f82a1f24bfc99bd9e8903c
ChristianStaubo <christianlewisstop@gmail.com>
2023-01-07 14:33:07 -0500

    added navbar layout and responsive design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,18 @@ function Navbar() {
     
     <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',width:'100%',height:'100%',justifyContent:'space-between'}}>
         <ArrowBackIosNewIcon sx={{ml:1}}/>
+
+        
         
         <Box sx={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <Box sx={{display:{xs:'none',md:'flex'},mr:10}}>
+            <Button>Premium</Button>
+            <Button>Support</Button>
+            <Button>Download</Button>
+        </Box>
             <Button>Sign up</Button>
             <Button>Log in</Button>
-            <MenuIcon sx={{ml:1}}/>
+            <MenuIcon sx={{ml:1,display:{xs:'block',md:'none'}}}/>
         </Box>
     </Box>
     </Box>

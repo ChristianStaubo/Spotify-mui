af814824fec8e645d34cdef6a5935b13eae01332
ChristianStaubo <christianlewisstop@gmail.com>
2023-01-07 15:03:29 -0500

    added assets for album covers on main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import { Box, Card, CardMedia, Typography } from '@mui/material'
+import peaceFulPianoImg from '../assets/spotifyPeacefulPiano.jpg'
 import React from 'react'
 
 function Main() {
@@ -9,21 +10,34 @@ function Main() {
         <Box sx={{pl:2,py:2}}>
             <Box sx={{display:'flex',justifyContent:'space-between'}}>
                 <Typography>Focus</Typography>
-                <Typography>SHOW ALL</Typography>
+                <Typography sx={{mr:1}}>SHOW ALL</Typography>
             </Box>
             {/* CARD SECTION */}
-            <Box>
-                <Card sx={{height:200,width:150,p:2}}>
-                    <CardMedia sx={{height:100}}
+            <Box sx={{display:'flex',gap:1}}>
+                <Card sx={{width:150,p:2}}>
+                    <CardMedia 
                     component='img'
                     
-                    src='#' />
+                    src={peaceFulPianoImg} />
                     <Box sx={{display:'flex',flexDirection:'column',gap:2,mt:2}}>
                         <Typography>Peaceful Piano</Typography>
                         <Typography variant='caption'>Relax and induldge with beautiful piano pieces</Typography>
                     </Box>
                     
                 </Card>
+
+                <Card sx={{width:150,p:2}}>
+                    <CardMedia 
+                    component='img'
+                    
+                    src={peaceFulPianoImg} />
+                    <Box sx={{display:'flex',flexDirection:'column',gap:2,mt:2}}>
+                        <Typography>Peaceful Piano</Typography>
+                        <Typography variant='caption'>Relax and induldge with beautiful piano pieces</Typography>
+                    </Box>
+                    
+                </Card>
+                
             </Box>
         </Box>
     </Box>

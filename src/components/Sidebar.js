ffa58e4166c604000efba02e870cb4b1e585f4d9
ChristianStaubo <christianlewisstop@gmail.com>
2023-01-08 17:03:47 -0500

    sidebar styling added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { Avatar, Box, Button, Typography } from '@mui/material'
+import HeadphonesIcon from '@mui/icons-material/Headphones';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import SetMealIcon from '@mui/icons-material/SetMeal';
@@ -10,52 +11,54 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <Box sx={{minWidth:'100px',width:'100px',display:'flex',flexDirection:'column',gap:4}}>
+    <Box sx={{minWidth:'120px',width:'120px',display:'flex',flexDirection:'column',gap:4,color:'white',backgroundColor:'black'}}>
         {/* Logo header */}
-        <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-            <HomeIcon fontSize='large'/>
-            <Typography>Spotify</Typography>
+        <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',mt:2,gap:0.5,'&:hover':{
+              cursor:'pointer'
+            }}}>
+            <HeadphonesIcon sx={{fontSize:'30px'}}/>
+            <Typography fontSize='medium'>Spotify</Typography>
         </Box>
 
 
         {/* Home search and you... section */}
         <Box >
-            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1}}>
-                <HomeIcon fontSize='large'/>
-                <Typography>Home</Typography>
+            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5}}>
+                <HomeIcon sx={{fontSize:'30px'}}/>
+                <Typography fontSize='small' sx={{fontWeight:'500'}}>Home</Typography>
             </Box>
-            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1}}>
-                <SearchIcon fontSize='large'/>
-                <Typography>Sea...</Typography>
+            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5}}>
+                <SearchIcon sx={{fontSize:'30px'}}/>
+                <Typography fontSize='small'>Sea...</Typography>
             </Box>
-            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1}}>
-                <PlaylistAddIcon fontSize='large' />
-                <Typography>You...</Typography>
+            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5}}>
+                <PlaylistAddIcon sx={{fontSize:'30px'}} />
+                <Typography fontSize='small'>You...</Typography>
             </Box>
             
         </Box>
 
         {/* Create like section */}
-        <Box sx={{pb:10}}>
-            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1}}>
-                    <Box sx={{backgroundColor:'gray'}}>
-                        <AddIcon fontSize='large'/>
+        <Box sx={{pb:10,display:'flex',flexDirection:'column',gap:1}}>
+            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:2}}>
+                    <Box sx={{backgroundColor:'#b2b2b2'}}>
+                        <AddIcon  sx={{color:'#595959',p:0.2,fontSize:'20px',}}/>
                     </Box>
                     
-                    <Typography>Cre...</Typography>
+                    <Typography fontSize='small'>Cre...</Typography>
                 </Box>
-                <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1}}>
-                    <Box sx={{backgroundColor:'blue'}}>
-                        <FavoriteIcon fontSize='large'/>
+                <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:2}}>
+                    <Box sx={{background: 'linear-gradient(to right bottom, #430089, #82ffa1)'}}>
+                        <FavoriteIcon  sx={{p:0.5,fontSize:'15px',pt:'6px'}}/>
                     </Box>
                     
-                    <Typography>Like...</Typography>
+                    <Typography fontSize='small'>Like..</Typography>
                 </Box>
         </Box>
 
 
         {/* Footer section */}
-        <Box sx={{display:'flex',flexDirection:'column',gap:2,fontSize:10,justifyContent:'center',ml:2}}>
+        <Box sx={{display:'flex',flexDirection:'column',gap:2,fontSize:10,justifyContent:'center',ml:2,color:'#959595'}}>
             <Typography sx={{fontSize:11}}>Legal</Typography>
 
             <Box>

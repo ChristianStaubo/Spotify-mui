0c4df4f7e0b90063c7421a9ff70578d7bf80235e
ChristianStaubo <christianlewisstop@gmail.com>
2023-01-08 21:17:32 -0500

    add final styling to footer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -23,61 +23,115 @@ function Sidebar() {
 
         {/* Home search and you... section */}
         <Box >
-            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5}}>
+            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5,'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>
                 <HomeIcon sx={{fontSize:'30px'}}/>
                 <Typography fontSize='small' sx={{fontWeight:'500'}}>Home</Typography>
             </Box>
-            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5}}>
+            <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5,'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>
                 <SearchIcon sx={{fontSize:'30px'}}/>
-                <Typography fontSize='small'>Sea...</Typography>
+                <Typography fontSize='small' sx={{'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>Sea...</Typography>
             </Box>
             <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:1,p:0.5}}>
                 <PlaylistAddIcon sx={{fontSize:'30px'}} />
-                <Typography fontSize='small'>You...</Typography>
+                <Typography fontSize='small' sx={{'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>You...</Typography>
             </Box>
             
         </Box>
 
         {/* Create like section */}
-        <Box sx={{pb:10,display:'flex',flexDirection:'column',gap:1}}>
+        <Box sx={{pb:10,display:'flex',flexDirection:'column',gap:1,'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>
             <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:2}}>
                     <Box sx={{backgroundColor:'#b2b2b2'}}>
                         <AddIcon  sx={{color:'#595959',p:0.2,fontSize:'20px',}}/>
                     </Box>
                     
-                    <Typography fontSize='small'>Cre...</Typography>
+                    <Typography fontSize='small' sx={{'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>Cre...</Typography>
                 </Box>
-                <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:2}}>
-                    <Box sx={{background: 'linear-gradient(to right bottom, #430089, #82ffa1)'}}>
-                        <FavoriteIcon  sx={{p:0.5,fontSize:'15px',pt:'6px'}}/>
+                <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:2,'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>
+                    <Box sx={{background: 'linear-gradient(to right bottom, #430089, #82ffa1)',display:'flex',justifyContent:'center'}}>
+                        <FavoriteIcon  sx={{p:0.5,fontSize:'15px'}}/>
                     </Box>
                     
-                    <Typography fontSize='small'>Like..</Typography>
+                    <Typography fontSize='small' sx={{'&:hover':{
+              cursor:'pointer',
+              fontWeight:'600'
+            }}}>Like..</Typography>
                 </Box>
         </Box>
 
 
         {/* Footer section */}
         <Box sx={{display:'flex',flexDirection:'column',gap:2,fontSize:10,justifyContent:'center',ml:2,color:'#959595'}}>
-            <Typography sx={{fontSize:11}}>Legal</Typography>
+            <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>Legal</Typography>
 
             <Box>
-                <Typography sx={{fontSize:11}}>Privacy</Typography>
-                <Typography sx={{fontSize:11}}>Center</Typography>
+                <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>Privacy</Typography>
+                <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>Center</Typography>
             </Box>
 
             <Box>
-                <Typography sx={{fontSize:11}}>Privacy</Typography>
-                <Typography sx={{fontSize:11}}>Policy</Typography>
+                <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>Privacy</Typography>
+                <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>Policy</Typography>
             </Box>
 
-            <Typography sx={{fontSize:11}}>Cookies</Typography>
+            <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>Cookies</Typography>
 
-            <Typography sx={{fontSize:11}}>About Ads</Typography>
+            <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              
+            }}}>About Ads</Typography>
 
-            <Typography sx={{fontSize:11}}>Your Privacy Choices</Typography>
+            <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              fontWeight:'400',
+              color:'white'
+              
+            }}}>Your Privacy Choices</Typography>
 
-            <Typography sx={{fontSize:11}}>Cookies</Typography>
+            <Typography sx={{fontSize:11,'&:hover':{
+              cursor:'pointer',
+              textDecoration:'underline'
+              
+            }}}>Cookies</Typography>
         </Box>
     </Box>
   )
